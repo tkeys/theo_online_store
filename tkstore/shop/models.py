042f0c547028378fefd606912477e5fd6c684299
Theokeys <theolarsenn@gmail.com>
2021-07-11 20:03:53 +0300

    Creating models
    This is setting up the models for the online shop

--- tkstore/shop/models.py
+++ tkstore/shop/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+# from django.core.urlresolvers import reverse
+
+
+# Create your models here
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True, unique=True)
+
+    class Meta:
+        verbose_name = "category"
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name="products")
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=100, db_index=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveBigIntegerField()
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
