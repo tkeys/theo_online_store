0cc58e2553b37a2562b639bf5fe293965f7b28fa
Theokeys <theolarsenn@gmail.com>
2021-07-11 21:50:39 +0300

    Adding models to Admin
    Adding the models to the admin page

--- tkstore/shop/models.py
+++ tkstore/shop/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
-
-# from django.core.urlresolvers import reverse
+from django.urls import reverse
 
 
 # Create your models here
@@ -10,20 +9,32 @@ class Category(models.Model):
     slug = models.SlugField(max_length=200, db_index=True, unique=True)
 
     class Meta:
+        ordering = ("name",)
         verbose_name = "category"
         verbose_name_plural = "categories"
 
     def __str__(self):
         return self.name
 
+    def get_absolute_url(self):
+        return reverse("shop:product_by_category", args=[self.slug])
+
 
 class Product(models.Model):
-    category = models.ForeignKey(Category, related_name="products")
+    category = models.ForeignKey(Category, related_name="products", on_delete=models.CASCADE)
     name = models.CharField(max_length=100, db_index=True)
     slug = models.SlugField(max_length=100, db_index=True)
     description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveBigIntegerField()
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = "Products"
+        ordering = ("-created",)
+
+    def __str__(self):
+        return self.name
