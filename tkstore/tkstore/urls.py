74b76df43f5205568a38ead340bc4d5e1e8ad080
Theokeys <theolarsenn@gmail.com>
2021-07-12 21:04:58 +0300

    create view
    creating the view

--- tkstore/tkstore/urls.py
+++ tkstore/tkstore/urls.py
@@ -18,6 +18,12 @@ from django.urls import path
 
 from django.conf import settings
 from django.conf.urls.static import static
+from shop.views import Category, product
+
+
+from django.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
