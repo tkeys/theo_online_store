5a4b56221a3cbb231e064aeade34737d8ac644e3
Theokeys <theolarsenn@gmail.com>
2021-07-12 20:02:37 +0300

    Admin
    creating the admin page

--- tkstore/shop/admin.py
+++ tkstore/shop/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
 from .models import Category, Product
 
+
 # Register your models here.
+# admin.site.register(Category)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    #prepopulated_fields = {'slug': ('name',)}
+
+
+#admin.site.register(Product)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'price', 'stock', 'available', 'created', 'updated']
+    #prepopulated_fields = {'slug': ('name',)}
--- tkstore/shop/models.py
+++ tkstore/shop/models.py
@@ -6,10 +6,10 @@ from django.urls import reverse
 # Create your models here
 class Category(models.Model):
     name = models.CharField(max_length=100, db_index=True)
-    slug = models.SlugField(max_length=200, db_index=True, unique=True)
+    #slug = models.SlugField(max_length=200, db_index=True, unique=True)
 
     class Meta:
-        ordering = ("name",)
+        ordering = ('name',)
         verbose_name = "category"
         verbose_name_plural = "categories"
 
@@ -17,13 +17,13 @@ class Category(models.Model):
         return self.name
 
     def get_absolute_url(self):
-        return reverse("shop:product_by_category", args=[self.slug])
+        return reverse('shop:product_by_category', args=[self.slug])
 
 
 class Product(models.Model):
     category = models.ForeignKey(Category, related_name="products", on_delete=models.CASCADE)
     name = models.CharField(max_length=100, db_index=True)
-    slug = models.SlugField(max_length=100, db_index=True)
+    #slug = models.SlugField(max_length=100, db_index=True)
     description = models.TextField(blank=True)
     image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
@@ -35,6 +35,10 @@ class Product(models.Model):
     class Meta:
         verbose_name_plural = "Products"
         ordering = ("-created",)
+        #index_together = (('id', 'slug'),)
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product_detail', args=[self.slug])
