c46aaad1fdfcb2572230f69220a3d3d0c2647e52
Theokeys <theolarsenn@gmail.com>
2021-07-17 17:39:46 +0300

    testing&Creating URLs and base html
    creating the base html and and url and testing

--- tkstore/shop/views.py
+++ tkstore/shop/views.py
@@ -1,16 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Category, Product
-#from django.views.generic import TemplateView, ListView, CreateView, DetailView, UpdateView, DeleteView, FormView
-#from django.urls import reverse_lazy
 
 
 # Create your views here.
+def contact_view(request, *args, **kwargs):
+    # return HttpResponse("<h1>This is my first project</h1>")
+    return render(request, "contact.html", {})
 
 
-def all_products(requests):
+#def all_categories(request):
+   # category = None
+   # categories = Category.objects.all()
+   # products = Product.objects.filter(available=True)
+    # if category_slug:
+    # category = get_object_or_404(Category, slug=category_slug)
+    # products = products.filter(category=category)
+    #return render(request, 'store.html', {'category': category,
+                                        #  'categories': categories,
+                                         # 'products': products})
+
+
+def all_products(request):
     products = Product.objects.all()
-    return render(requests, 'home.html', {'products': products})
+    return render(request, 'home.html', {'products': products})
 
 
-def product_detail(request):
+# adding view to display single product
+#def product_detail(request, slug):
+    #product = get_object_or_404(Product, slug=slug, available=True)
+    #cart_product_form = CartAddproductForm()
+    #return render(request,
+                  #'detail.html',
+                  #{'product': product, 'cart_product_form': cart_product_form})
--- tkstore/tkstore/urls.py
+++ tkstore/tkstore/urls.py
@@ -17,14 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-
-
-
-
-from django.urls import include
-from django.conf import settings
-from django.conf.urls.static import static
+from shop import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('contact/', views.contact_view, name='contact'),
+    path('', views.all_products, name='all_products'),
+
 ]
